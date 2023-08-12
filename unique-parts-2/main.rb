19789f4ba4a0dd654e685fdbae821d03c0025991
necocoa <joyds217@gmail.com>
2023-08-12 17:40:17 +0900

    refactor methods

--- unique-parts-2/main.rb
+++ unique-parts-2/main.rb
@@ -12,9 +12,7 @@ class UniquePaths
   end
 
   def call
-    if invalid_area?(0, 0)
-      return
-    end
+    return if !valid_area?(0, 0)
 
     @count += search(0, 0)
   end
@@ -22,27 +20,17 @@ class UniquePaths
   private
 
   def search(x, y)
-    # p "search x: #{x}, y: #{y}"
-
     if goal?(x, y)
       cache_count!(x, y, 1)
       return 1
     end
 
-    count = 0
-    next_position(x, y).each do |next_x, next_y|
-      # p "next_x: #{next_x}, next_y: #{next_y}"
-
-      cache = cache_count(next_x, next_y)
-      if cache.nil?
-        count += search(next_x, next_y)
-      else
-        count += cache
-      end
+    count = next_position(x, y).sum do |next_x, next_y|
+      cache_count(next_x, next_y) || search(next_x, next_y)
     end
 
     cache_count!(x, y, count)
-    count
+    return count
   end
 
   def next_position(x, y)
@@ -50,13 +38,13 @@ class UniquePaths
 
     # 右に移動
     next_x = x+1
-    if next_x <= @max_x && @grid[y][next_x] == 0
+    if next_x <= @max_x && valid_area?(next_x, y)
       next_pos << [next_x, y]
     end
 
     # 下に移動
     next_y = y+1
-    if next_y <= @max_y && @grid[next_y][x] == 0
+    if next_y <= @max_y && valid_area?(x, next_y)
       next_pos << [x, next_y]
     end
 
@@ -64,19 +52,19 @@ class UniquePaths
   end
 
   def goal?(x, y)
-    x == @max_x && y == @max_y && !invalid_area?(x, y)
+    x == @max_x && y == @max_y && valid_area?(x, y)
   end
 
-  def invalid_area?(x, y)
-    @grid[y][x] == 1
-  end
-
-  def cache_count!(x, y, count)
-    @cache_count["#{x}-#{y}".to_sym] = count
+  def valid_area?(x, y)
+    @grid[y][x] == 0
   end
 
   def cache_count(x, y)
     @cache_count["#{x}-#{y}".to_sym]
+  end
+
+  def cache_count!(x, y, count)
+    @cache_count["#{x}-#{y}".to_sym] = count
   end
 end
 
