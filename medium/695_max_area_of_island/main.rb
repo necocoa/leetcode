a38781a5c018f0a46fc879f9867588cc74727bc9
necocoa <joyds217@gmail.com>
2023-08-13 21:42:09 +0900

    refacrot

--- medium/695_max_area_of_island/main.rb
+++ medium/695_max_area_of_island/main.rb
@@ -5,24 +5,21 @@
 #
 
 # @lc code=start
+
 class MaxAreaOfIsland
-  attr_reader :grid, :max_x, :max_y, :max_area
-  attr_accessor :checked_grid
+  attr_reader :max_area
 
   def initialize(grid)
     @grid = grid
-    x_length = grid[0].length
-    y_length = grid.length
-    @checked_grid = init_checked_grid(x_length, y_length)
-    @max_x = x_length - 1
-    @max_y = y_length - 1
+    @x_length = grid[0].length
+    @y_length = grid.length
+    @checked_grid = init_checked_grid(@x_length, @y_length)
     @max_area = 0
   end
 
   def call
-    grid.each_with_index do |rows, y|
-      rows.each_with_index do |_col, x|
-        # p "grid x: #{x}, y: #{y}"
+    @grid.each_with_index do |rows, y|
+      rows.each_with_index do |col, x|
         @count = 0
         search_area(x, y)
       end
@@ -36,67 +33,45 @@ class MaxAreaOfIsland
     Array.new(y_length) { Array.new(x_length, false) }
   end
 
-  def checked!(x, y)
-    checked_grid[y][x] = true
-  end
-
-  def checked?(x, y)
-    checked_grid[y][x]
-  end
-
   def search_area(x, y)
     return if checked?(x, y)
 
     checked!(x, y)
 
-    return if grid[y][x].zero?
+    return if @grid[y][x] == 0
 
     @count += 1
 
     calc_around_positions(x, y).each do |x, y|
-      # p "positions x: #{x}, y: #{y}"
       search_area(x, y)
     end
 
     if @max_area < @count
-      # p "@max_area: #{@max_area}, c_count: #{c_count}"
       @max_area = @count
     end
   end
 
   # 指定した位置の周辺の位置を返す
-  def calc_around_positions(base_x, base_y)
-    ary = []
-
-    calc_x = base_x - 1
-    calc_y = base_y
-    unless out_of_area(calc_x, calc_y)
-      ary << [calc_x, calc_y]
+  def calc_around_positions(x, y)
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+    4.times.filter_map do |i|
+      next_x = x + dx[i]
+      next_y = y + dy[i]
+      [next_x, next_y] if !out_of_area?(next_x, next_y)
     end
-
-    calc_x = base_x + 1
-    calc_y = base_y
-    unless out_of_area(calc_x, calc_y)
-      ary << [calc_x, calc_y]
-    end
-
-    calc_x = base_x
-    calc_y = base_y - 1
-    unless out_of_area(calc_x, calc_y)
-      ary << [calc_x, calc_y]
-    end
-
-    calc_x = base_x
-    calc_y = base_y + 1
-    unless out_of_area(calc_x, calc_y)
-      ary << [calc_x, calc_y]
-    end
-
-    ary
   end
 
-  def out_of_area(x, y)
-    x.negative? || y.negative? || x > @max_x || y > @max_y
+  def out_of_area?(x, y)
+    x < 0 || y < 0 || x >= @x_length || y >= @y_length
+  end
+
+  def checked!(x, y)
+    @checked_grid[y][x] = true
+  end
+
+  def checked?(x, y)
+    @checked_grid[y][x]
   end
 end
 
