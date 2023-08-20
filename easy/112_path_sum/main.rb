88b184deada4224d3cc6147f2aeb7d6a71d5de6c
necocoa <joyds217@gmail.com>
2023-08-20 16:58:15 +0900

    refacrot

--- easy/112_path_sum/main.rb
+++ easy/112_path_sum/main.rb
@@ -20,40 +20,32 @@ end
 # @param {Integer} target_sum
 # @return {Boolean}
 def has_path_sum(root, target_sum)
-  hash = { ans: false }
-  current_sum = 0
   return false if root.nil?
 
-  dfs(root, target_sum, current_sum, hash)
-  hash[:ans]
+  dfs(root, target_sum, 0)
 end
 
-def dfs(node, target_sum, current_sum, hash)
-  # 答えが出てたら終了
-  return if hash[:ans] == true
+def dfs(node, target_sum, current_sum)
+  current_sum += node.val
 
   # 末端到達
   if node.left.nil? && node.right.nil?
-    if current_sum + node.val == target_sum
-      hash[:ans] = true
-    end
-    return
+    return current_sum == target_sum
   end
 
-  dfs(node.left, target_sum, current_sum + node.val, hash) if !node.left.nil?
-  dfs(node.right, target_sum, current_sum + node.val, hash) if !node.right.nil?
+  if !node.left.nil? && dfs(node.left, target_sum, current_sum)
+    return true
+  end
+
+  if !node.right.nil? && dfs(node.right, target_sum, current_sum)
+    return true
+  end
+
+  false
 end
 # @lc code=end
 
-root = TreeNode.new(5,
-                    TreeNode.new(4,
-                                 TreeNode.new(11,
-                                              TreeNode.new(7),
-                                              TreeNode.new(2))),
-                    TreeNode.new(8,
-                                 TreeNode.new(13),
-                                 TreeNode.new(4,
-                                              nil,
-                                              TreeNode.new(1))))
+root = TreeNode.new(5, TreeNode.new(4, TreeNode.new(11, TreeNode.new(7), TreeNode.new(2))),
+                    TreeNode.new(8, TreeNode.new(13), TreeNode.new(4, nil, TreeNode.new(1))))
 target_sum = 22
 p has_path_sum(root, target_sum)
