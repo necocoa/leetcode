db496f8ddfe0136e2b1fbbfbb48e3d7b7ad36976
necocoa <joyds217@gmail.com>
2023-09-27 00:52:44 +0900

    refactor

--- medium/12_integer_to_roman/main.go
+++ medium/12_integer_to_roman/main.go
@@ -14,31 +14,14 @@ import (
 
 func intToRoman(num int) string {
 	roman := []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
-	romanMap := map[string]int{
-		"I":  1,
-		"IV": 4,
-		"V":  5,
-		"IX": 9,
-		"X":  10,
-		"XL": 40,
-		"L":  50,
-		"XC": 90,
-		"C":  100,
-		"CD": 400,
-		"D":  500,
-		"CM": 900,
-		"M":  1000,
-	}
+	value := []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
 
 	var ans string
 
 	for i := len(roman) - 1; i >= 0; i-- {
-		roman := roman[i]
-		romanInt, _ := romanMap[roman]
-
-		for num >= romanInt {
-			ans += roman
-			num -= romanInt
+		for num >= value[i] {
+			ans += roman[i]
+			num -= value[i]
 		}
 	}
 
