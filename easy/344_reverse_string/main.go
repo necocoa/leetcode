fcb0acf5149b373cefcf4bba6995a5e8083d38b2
necocoa <joyds217@gmail.com>
2023-10-22 15:33:22 +0900

    challenge 344

---
+++ easy/344_reverse_string/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/*
+ * @lc app=leetcode id=344 lang=golang
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+func reverseString(s []byte) {
+	for i := 0; i < len(s)/2; i++ {
+		s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
+	}
+	fmt.Println(s)
+	return
+}
+
+// @lc code=end
+
+func main() {
+	reverseString([]byte("hello"))
+}
